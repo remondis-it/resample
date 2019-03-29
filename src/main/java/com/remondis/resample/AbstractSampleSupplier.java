51f5a0db40b1d2dc8b5066c5048b56fabbaa8233
Christopher Schuette <christopher.schuette@remondis.de>
2019-03-29 12:37:24 +0100

    Documentation and ReadMe Update

--- src/main/java/com/remondis/resample/AbstractSampleSupplier.java
+++ src/main/java/com/remondis/resample/AbstractSampleSupplier.java
@@ -9,7 +9,7 @@ import static java.util.Objects.requireNonNull;
  */
 public abstract class AbstractSampleSupplier<T> implements SampleSupplier<T> {
 
-        private Class<T> type;
+  private Class<T> type;
 
   public AbstractSampleSupplier(Class<T> type) {
     super();
