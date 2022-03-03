c03fc71e00f8e728ebccac74a65b5c6a43725088
Christopher Schütte <cschue88@googlemail.com>
2022-03-03 16:59:34 +0100

    Release 0.0.11

--- src/main/java/com/remondis/resample/InvocationSensor.java
+++ src/main/java/com/remondis/resample/InvocationSensor.java
@@ -24,78 +24,73 @@ import static net.bytebuddy.matcher.ElementMatchers.not;
  */
 class InvocationSensor<T> {
 
-    private T proxyObject;
+  private T proxyObject;
 
-    private List<String> propertyNames = new LinkedList<>();
+  private List<String> propertyNames = new LinkedList<>();
 
-    InvocationSensor(Class<T> superType) {
-        Class<? extends T> proxyClass = new ByteBuddy().subclass(superType)
-                                                       .method(isGetter())
-                                                       .intercept(of((proxy, method, args) -> markPropertyAsCalled(
-                                                               method)))
-                                                       .method(isDeclaredBy(Object.class))
-                                                       .intercept(of((proxy, method, args) -> invokeMethodProxySafe(
-                                                               method,
-                                                               this,
-                                                               args)))
-                                                       .method(not(isGetter()).and(not(isDeclaredBy(Object.class))))
-                                                       .intercept(of((proxy, method, args) -> {
-                                                           throw ReflectionException.notAGetter(method);
-                                                       }))
-                                                       .make()
-                                                       .load(getClass().getClassLoader(),
-                                                             ClassLoadingStrategy.Default.INJECTION)
-                                                       .getLoaded();
+  InvocationSensor(Class<T> superType) {
+    Class<? extends T> proxyClass = new ByteBuddy().subclass(superType)
+        .method(isGetter())
+        .intercept(of((proxy, method, args) -> markPropertyAsCalled(method)))
+        .method(isDeclaredBy(Object.class))
+        .intercept(of((proxy, method, args) -> invokeMethodProxySafe(method, this, args)))
+        .method(not(isGetter()).and(not(isDeclaredBy(Object.class))))
+        .intercept(of((proxy, method, args) -> {
+          throw ReflectionException.notAGetter(method);
+        }))
+        .make()
+        .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.INJECTION)
+        .getLoaded();
 
-        proxyObject = superType.cast(ReflectionUtil.newInstance(proxyClass));
+    proxyObject = superType.cast(ReflectionUtil.newInstance(proxyClass));
+  }
+
+  private Object markPropertyAsCalled(Method method) {
+    String propertyName = toPropertyName(method);
+    propertyNames.add(propertyName);
+    return nullOrDefaultValue(method.getReturnType());
+  }
+
+  /**
+   * Returns the proxy object get-method calls can be performed on.
+   *
+   * @return The proxy.
+   */
+  T getSensor() {
+    return proxyObject;
+  }
+
+  /**
+   * Returns the list of property names that were tracked by get calls.
+   *
+   * @return Returns the tracked property names.
+   */
+  List<String> getTrackedPropertyNames() {
+    return Collections.unmodifiableList(propertyNames);
+  }
+
+  /**
+   * Checks if there were any properties accessed by get calls.
+   *
+   * @return Returns <code>true</code> if there were at least one interaction with
+   *         a property. Otherwise <code>false</code> is returned.
+   */
+  boolean hasTrackedProperties() {
+    return !propertyNames.isEmpty();
+  }
+
+  /**
+   * Resets all tracked information.
+   */
+  void reset() {
+    propertyNames.clear();
+  }
+
+  private static Object nullOrDefaultValue(Class<?> returnType) {
+    if (returnType.isPrimitive()) {
+      return defaultValue(returnType);
+    } else {
+      return null;
     }
-
-    private Object markPropertyAsCalled(Method method) {
-        String propertyName = toPropertyName(method);
-        propertyNames.add(propertyName);
-        return nullOrDefaultValue(method.getReturnType());
-    }
-
-    /**
-     * Returns the proxy object get-method calls can be performed on.
-     *
-     * @return The proxy.
-     */
-    T getSensor() {
-        return proxyObject;
-    }
-
-    /**
-     * Returns the list of property names that were tracked by get calls.
-     *
-     * @return Returns the tracked property names.
-     */
-    List<String> getTrackedPropertyNames() {
-        return Collections.unmodifiableList(propertyNames);
-    }
-
-    /**
-     * Checks if there were any properties accessed by get calls.
-     *
-     * @return Returns <code>true</code> if there were at least one interaction with
-     * a property. Otherwise <code>false</code> is returned.
-     */
-    boolean hasTrackedProperties() {
-        return !propertyNames.isEmpty();
-    }
-
-    /**
-     * Resets all tracked information.
-     */
-    void reset() {
-        propertyNames.clear();
-    }
-
-    private static Object nullOrDefaultValue(Class<?> returnType) {
-        if (returnType.isPrimitive()) {
-            return defaultValue(returnType);
-        } else {
-            return null;
-        }
-    }
+  }
 }
